03336580576aef43b96f57c17bdac15cce3e1230
R4HANDON <44488041+rondinelle03@users.noreply.github.com>
2021-06-21 08:57:36 -0300

    Montagem htlm finalizado

--- landpage/libs/js/geral.js
+++ landpage/libs/js/geral.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
+$('#cus-destaques').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
 
 $('.navbar  a').click(function(){
 
